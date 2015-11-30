47de46b926823ac5e58165b42e28c94178db0422
afik <k.afis3@rocketmail.com>
2015-11-30 17:18:38 +0700

    add generator

---
+++ dsl-generator/src/dsl/generator/Main.java
@@ -0,0 +1,19 @@
+package dsl.generator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TOLEP
+ */
+public class Main {
+    public static void main(String[] args) {
+        Execute ex = new Execute();
+        String output = ex.foo();
+        System.out.println(output);
+    }
+}
