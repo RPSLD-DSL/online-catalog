2a8c2de17d6e4ace99dc85f9dc1afb62ee53c948
Imballinst <lucent_rays@yahoo.com>
2015-11-30 13:24:00 +0700

    groovy v2

--- dsl-generator/src/dsl/katalogtemplate/KatalogDsl.groovy
+++ dsl-generator/src/dsl/katalogtemplate/KatalogDsl.groovy
@@ -3,14 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dsl.generator;
+
+package dsl.memotemplate
 
 /**
  *
  * @author ASUS
  */
-public class MainGenerator {
-    public static void main(String[] args) {
-        
-    }
+import groovy.xml.MarkupBuilder
+
+/**
+ * Processes a simple DSL to create various formats of a memo: xml, html, and text
+ */
+class KatalogDsl {
+    //todo
 }
+
---
+++ dsl-generator/src/dsl/katalogtemplate/Parser.groovy
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dsl.katalogtemplate;
+
+/**
+ *
+ * @author ASUS
+ */
+class Parser {
+    public void read(String filename) {
+        String input = System.getProperty("user.dir") + "\\src\\dsl\\input\\" + filename;
+        
+    }
+    
+    public void validate(String filename) {
+        
+    }
+}
+
