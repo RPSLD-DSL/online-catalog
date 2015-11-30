b145d7c9e29e4fcb9f3e925f3d26ce50a2a73805
Imballinst <lucent_rays@yahoo.com>
2015-11-30 17:11:28 +0700

    gara2 afik

---
+++ dsl-generator/src/dsl/katalogtemplate/Section.groovy
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dsl.memotemplate
+
+/**
+ *
+ * @author ASUS
+ */
+class Section {
+    String title
+    String body
+}
+
