20a55dcacd4b7a11efe51826920d422a6c546571
Imballinst <lucent_rays@yahoo.com>
2015-11-30 13:16:33 +0700

    groovy

---
+++ dsl-generator/src/dsl/generator/MainGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsl.generator;
+
+import dsl.memotemplate.MemoDsl;
+import dsl.memotemplate.Section;
+import groovy.lang.Closure;
+import groovy.util.GroovyTestCase;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MainGenerator {
+    public static void main(String[] args) {
+        MemoDsl dsl = new MemoDsl();
+        
+    }
+}
